6d0f0d96d2f52412bdbae42f07e337e906371ef7
Wixonic <contact@wixonic.fr>
2025-08-19 18:01:20 +0200

    Update

--- src/website/handlers/obs.js
+++ src/website/handlers/obs.js
@@ -25,6 +25,15 @@ const updateDeviceList = () => {
 	for (const match of output[1].matchAll(/\.*\] \[(\d+)\] (.+)/g)) deviceList.audio[Number(match[1])] = trimName(match[2]);
 };
 
+const defaultArgs = [
+	"-hide_banner",
+	"-loglevel", "warning",
+
+	"-fflags", "nobuffer+genpts",
+	"-flags", "low_delay",
+	"-max_muxing_queue_size", "1024"
+];
+
 /**
  * @type {{[name: string]: {spawn: (logger: import("@wixonic/logger").Logger, settings: import("../../types.d.ts").Settings) => childProcess.ChildProcess, process: childProcess.ChildProcess?, active: boolean, name: string}}}
  */
@@ -35,11 +44,7 @@ const captureProcess = {
 		spawn: (logger, settings) => {
 			logger.info("Starting process:", captureProcess.audio.name);
 			return childProcess.spawn("/usr/local/ffmpeg-4.1/bin/ffmpeg", [
-				"-hide_banner",
-				"-loglevel", "warning",
-
-				"-flags", "low_delay",
-				"-fflags", "nobuffer",
+				...defaultArgs,
 
 				"-f", "avfoundation",
 				"-framerate", "60",
@@ -50,8 +55,6 @@ const captureProcess = {
 				"-ac", "2",
 				"-ar", "48000",
 				"-af", "volume=0.5",
-
-				"-tune", "zerolatency",
 
 				"-flush_packets", "1",
 				"-muxdelay", "0",
@@ -68,11 +71,7 @@ const captureProcess = {
 		spawn: (logger, settings) => {
 			logger.info("Starting process:", captureProcess.screenshare1.name);
 			return childProcess.spawn("/usr/local/ffmpeg-4.1/bin/ffmpeg", [
-				"-hide_banner",
-				"-loglevel", "warning",
-
-				"-flags", "low_delay",
-				"-fflags", "nobuffer",
+				...defaultArgs,
 
 				"-f", "avfoundation",
 				"-capture_cursor", "1",
@@ -103,11 +102,7 @@ const captureProcess = {
 		spawn: (logger, settings) => {
 			logger.info("Starting process:", captureProcess.screenshare2.name);
 			return childProcess.spawn("/usr/local/ffmpeg-4.1/bin/ffmpeg", [
-				"-hide_banner",
-				"-loglevel", "warning",
-
-				"-flags", "low_delay",
-				"-fflags", "nobuffer",
+				...defaultArgs,
 
 				"-f", "avfoundation",
 				"-capture_cursor", "1",
@@ -138,11 +133,7 @@ const captureProcess = {
 		spawn: (logger, settings) => {
 			logger.info("Starting process:", captureProcess.camera.name);
 			return childProcess.spawn("/usr/local/ffmpeg-4.1/bin/ffmpeg", [
-				"-hide_banner",
-				"-loglevel", "warning",
-
-				"-fflags", "nobuffer+genpts",
-				"-flags", "low_delay",
+				...defaultArgs,
 
 				"-probesize", "32M",
 				"-analyzeduration", "30M",
@@ -158,7 +149,6 @@ const captureProcess = {
 				"-an",
 				"-c:v", "libx264",
 				"-preset", "ultrafast",
-				"-tune", "zerolatency",
 				"-crf", "25",
 				"-profile:v", "baseline",
 				"-level", "4.1",
@@ -182,11 +172,7 @@ const captureProcess = {
 		spawn: (logger, settings) => {
 			logger.info("Starting process:", captureProcess.microphone.name);
 			return childProcess.spawn("ffplay", [
-				"-hide_banner",
-				"-loglevel", "warning",
-
-				"-flags", "low_delay",
-				"-fflags", "nobuffer",
+				...defaultArgs,
 
 				"-autoexit",
 				"-nodisp",
@@ -261,7 +247,7 @@ const info = {
 						cp.process.on("error", (error) => logger.warn("Child Process Error:", error));
 					}
 				} else if (!cp.process.killed && !cp.active) {
-					logger.info("Killing process:", cp.process.name);
+					logger.info("Killing process:", cp.name);
 					cp.process.kill("SIGTERM");
 				}
 			}
