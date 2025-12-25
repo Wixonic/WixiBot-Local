fe81c11dab9d62c0d1943546b04665cedd05f3d6
Wixonic <contact@wixonic.fr>
2025-12-25 11:48:30 +0100

    feat: Add deep cloning utility and implement robust error handling with process exit on uncaught exceptions/rejections.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,17 @@
+const clone = (obj, cloned = new WeakMap()) => {
+	if (obj === null || typeof obj !== "object") return obj;
+	if (cloned.has(obj)) return cloned.get(obj);
+
+	const clonedObj = Array.isArray(obj) ? [] : {};
+	cloned.set(obj, clonedObj);
+
+	for (const key in obj) {
+		if (Object.prototype.hasOwnProperty.call(obj, key)) clonedObj[key] = clone(obj[key], cloned);
+	}
+
+	return clonedObj;
+};
+
 /**
  * @param {number} milliseconds
  * @returns {Promise<void>}
@@ -5,5 +19,6 @@
 const wait = (milliseconds) => new Promise((resolve) => setTimeout(() => resolve(), milliseconds));
 
 module.exports = {
+	clone,
 	wait
 };
--- src/process.js
+++ src/process.js
@@ -4,11 +4,34 @@ const Server = require("./lib/server.js");
 
 const settings = require("./settings.js");
 
+const { clone } = require("./lib/utils.js");
+
 log.displayDate = false;
 
-const main = async () => {
-	const server = new Server(log, settings);
-	await server.init();
+const main = async (logger) => {
+	const handler = clone(logger);
+	handler.error = (...any) => {
+		let stacks = "";
+		any.forEach((el) => {
+			if (el instanceof Error) {
+				stacks += el.stack;
+				el = `${el.name}: ${el.message}`;
+			}
+		});
+		logger.error(...any);
+		if (stacks.length > 0) logger.debug("{Stack}", stacks.replaceAll("\n", "<br />"));
+		process.exit(1);
+	};
+
+	process.on("uncaughtException", (e) => handler.error("Uncaught exception:", e));
+	process.on("unhandledRejection", (e) => handler.error("Unhandled rejection:", e));
+
+	try {
+		const server = new Server(log, settings);
+		await server.init();
+	} catch (e) {
+		handler.error(e);
+	}
 };
 
-main();
+main(log);
