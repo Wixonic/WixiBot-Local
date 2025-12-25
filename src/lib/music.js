23bdc98a16fa4af2e71e0a7fa272096eb0688e2f
Wixonic <contact@wixonic.fr>
2025-12-25 11:10:37 +0100

    feat: Add error handling to Music.app AppleScript for player position and track duration, and handle the new ERROR state in JavaScript.

--- src/lib/music.js
+++ src/lib/music.js
@@ -4,28 +4,37 @@ const getCurrentTrackInfo = () => new Promise((resolve) => {
 	const script = `
 if application "Music" is running then
 	tell application "Music"
-		set playerState to player state
-		if playerState is playing then
-			set currentTrack to current track
-			set trackName to name of currentTrack
-			set artistName to artist of currentTrack
-			set albumName to album of currentTrack
-			set startedAt to player position
-			set trackDuration to duration of currentTrack
-			
-			return {"PLAYING", trackName, artistName, albumName, startedAt, trackDuration}
-		else if playerState is paused then
-			set currentTrack to current track
-			set trackName to name of currentTrack
-			set artistName to artist of currentTrack
-			set albumName to album of currentTrack
-			set startedAt to player position
-			set trackDuration to duration of currentTrack
-			
-			return {"PAUSED", trackName, artistName, albumName, startedAt, trackDuration}
-		else
-			return {"STOPPED", "", "", "", 0, 0}
-		end if
+		try
+			set playerState to player state
+			if playerState is playing or playerState is paused then
+				set currentTrack to current track
+				set trackName to name of currentTrack
+				set artistName to artist of currentTrack
+				set albumName to album of currentTrack
+				
+				try
+					set startedAt to player position
+				on error
+					set startedAt to 0
+				end try
+				
+				try
+					set trackDuration to duration of currentTrack
+				on error
+					set trackDuration to 0
+				end try
+				
+				if playerState is playing then
+					return {"PLAYING", trackName, artistName, albumName, startedAt, trackDuration}
+				else
+					return {"PAUSED", trackName, artistName, albumName, startedAt, trackDuration}
+				end if
+			else
+				return {"STOPPED", "", "", "", 0, 0}
+			end if
+		on error
+			return {"ERROR", "", "", "", 0, 0}
+		end try
 	end tell
 else
 	return {"STOPPED", "", "", "", 0, 0}
@@ -40,6 +49,8 @@ end if`;
 				resolve(undefined);
 			} else if (result[0] === "STOPPED") {
 				resolve(null);
+			} else if (result[0] === "ERROR") {
+				resolve(undefined);
 			} else {
 				const [
 					state,
